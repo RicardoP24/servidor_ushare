29ca42cb23888fc66668295b00b2ecf95dc3cf1a
RicardoP24 <ricardopilartes03@gmail.com>
2024-06-26 16:09:22 +0100

    vercel

--- index.js
+++ index.js
@@ -10,23 +10,15 @@ const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 const secretKey = process.env.JWT_SECRET; // Use default key if not provided in the environment
 
-// Configuração do pool de conexão com o PostgreSQL
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  // ssl: {
-  //   // You can specify SSL options here
-  //   //rejectUnauthorized: true, // Set to true to reject unauthorized connections
-  //   // Optionally, you can provide SSL certificate details
-  //   // For example:
-  //    ca: fs.readFileSync('./certificadoSSL/global-bundle.pem'), // Path to CA certificate file
-  //   // cert: fs.readFileSync('/path/to/client-certificate.crt'), // Path to client certificate file
-  //   // key: fs.readFileSync('/path/to/client-key.key'), // Path to client key file
-  // },
-});
+  connectionString: process.env.POSTGRES_URL,
+})
+
+pool.connect((err)=>{
+  if(err) throw err;
+  console.log("Conectado a base de dados")
+
+})
 
 // Middleware para parsear o corpo da requisição como JSON
 app.use(express.json());
@@ -59,12 +51,14 @@ app.post('/login', async (req, res) => {
     // Gera o token JWT
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
 
-    res.status(200).json({ token });
+    res.status(200).json({user,token });
   } catch (error) {
     console.error('Erro ao fazer login:', error);
     res.status(500).json({ message: 'Erro interno do servidor' });
   }
 });
+
+
 app.get('/protected', verifyToken, (req, res) => {
   const userId = req.user.id;
 
@@ -141,6 +135,21 @@ app.get('/municipios', async (req, res) => {
     res.status(500).send('Erro interno do servidor');
   }
 });
+
+app.post('/anuncios', async (req, res) => {
+  const { id_Munic, id_user, titulo, TipoAnuncio, linkImagem, descricao, Estado } = req.body;
+  try {
+    const result = await pool.query(
+      'INSERT INTO anuncios (id_munic, id_user, titulo, datadepub, tipoanuncio, linkimagem, descricao, estado) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+      [id_Munic, id_user, titulo, new Date(), TipoAnuncio, linkImagem, descricao, Estado]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
+});
+
 
 
 app.listen(PORT, () => {
